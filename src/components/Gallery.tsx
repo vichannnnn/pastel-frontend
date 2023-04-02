e1c5069bcc53eedafa3709f2335d9cf32f4c829d
Eugene Loo(Eugene) <eugenell@yoozoo.com>
2023-04-02 19:29:30 +0800

    update

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -20,20 +20,17 @@ const Gallery: React.FC<GalleryProps> = ({ images, isLoading }) => {
     '2xl': 5,
   });
 
-  const imageWidth = useBreakpointValue({ base: '100%', sm: '400px' }) || '400px';
-  const imageHeight = useBreakpointValue({ base: 'auto', sm: '500px' }) || '500px';
-
   const renderImages = () => {
     return images.map((image) => (
       <Image
         key={image.id}
         src={image.src}
         alt={image.alt}
-        width={imageWidth}
-        height={imageHeight}
+        width="auto"
+        height="auto"
         prompt={image.prompt}
       />
-    ));     
+    ));
   };
 
   return (
